fa58b16b9deff992f987ed752db61172e743cef5
abnerwerley <abner.silva36@aluno.unip.br>
2023-11-16 18:09:25 -0300

    ETS-1: Realizacao de boas vindas e login

--- main.c
+++ main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+struct LoginStruct
 {
-    printf("Hello, World!\n");
+    char nome[50];
+    char senha[50];
+};
+
+int verificarLogin(struct LoginStruct usuarios[], int numUsuarios, char nome[], char senha[])
+{
+    for (int i = 0; i < numUsuarios; i++)
+    {
+        if (strcmp(usuarios[i].nome, nome) == 0 && strcmp(usuarios[i].senha, senha) == 0)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
+
+int realizarLogin()
+{
+    struct LoginStruct usuarios[2];
+    strcpy(usuarios[0].nome, "admin");
+    strcpy(usuarios[0].senha, "admin123");
+    strcpy(usuarios[1].nome, "cliente1");
+    strcpy(usuarios[1].senha, "senha1234");
+
+    char nome[50];
+    char senha[50];
+
+    printf("Nome de usuario: ");
+    scanf("%s", nome);
+
+    printf("Senha: ");
+    scanf("%s", senha);
+
+    if (verificarLogin(usuarios, 2, nome, senha))
+    {
+        printf("Login bem-sucedido. Bem-vindo, %s!\n", nome);
+        return 1;
+    }
+    else
+    {
+        printf("Login falhou. Verifique seu nome de usuario e senha.\n");
+        return 0;
+    }
+
+}
+int main()
+{
+
+    printf("Seja bem-vindo(a) a Eco Tech Solutions, para comecar vamos ao seu login:\n");
+    realizarLogin();
+    return 0;
+}
+
+
