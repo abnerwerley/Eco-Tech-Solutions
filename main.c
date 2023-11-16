db72dc9c2ad48c2b365f82a3c1ebf257cdd58f32
abnerwerley <abner.silva36@aluno.unip.br>
2023-11-16 19:38:20 -0300

    ETS-2: Menu inicial para admin e clientes

--- main.c
+++ main.c
@@ -1,21 +1,80 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
+int quantidadeUsuarios = 2;
+bool ehAdmin;
 struct LoginStruct
 {
     char nome[50];
     char senha[50];
 };
 
-int verificarLogin(struct LoginStruct usuarios[], int numUsuarios, char nome[], char senha[])
+int verificarLogin(struct LoginStruct usuarios[], char nome[], char senha[])
 {
-    for (int i = 0; i < numUsuarios; i++)
+    for (int i = 0; i < quantidadeUsuarios; i++)
     {
         if (strcmp(usuarios[i].nome, nome) == 0 && strcmp(usuarios[i].senha, senha) == 0)
         {
+            ehAdmin = verificaSeEhAdmin(usuarios[i]);
             return 1;
         }
     }
+    return 0;
+}
+
+int verificaSeEhAdmin(struct LoginStruct usuario)
+{
+    if (strcmp(usuario.nome, "admin") == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int menuInicial(bool ehAdmin)
+{
+
+    int opcao;
+
+    printf("\nMenu Inicial:\n");
+    printf("1. Cadastrar Industria\n");
+    printf("2. Atualizar Dados\n");
+    printf("3. Relatorios\n");
+    printf("4. Gerenciamento\n");
+    if(ehAdmin)
+    {
+        printf("5. Cadastrar Cliente\n");
+    }
+    printf("0. Sair\n");
+
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        cadastrarIndustria();
+        break;
+    case 2:
+        atualizarDados();
+        break;
+    case 3:
+        gerarRelatorios();
+        break;
+    case 4:
+        gerenciar();
+        break;
+    case 5:
+        cadastrarCliente();
+        break;
+    case 0:
+        printf("Saindo do programa. Ate logo!\n");
+        break;
+    default:
+        printf("Opção inválida. Tente novamente.\n");
+    }
+
     return 0;
 }
 
@@ -23,9 +82,9 @@ int realizarLogin()
 {
     struct LoginStruct usuarios[2];
     strcpy(usuarios[0].nome, "admin");
-    strcpy(usuarios[0].senha, "admin123");
+    strcpy(usuarios[0].senha, "admin");
     strcpy(usuarios[1].nome, "cliente1");
-    strcpy(usuarios[1].senha, "senha1234");
+    strcpy(usuarios[1].senha, "senha123");
 
     char nome[50];
     char senha[50];
@@ -36,9 +95,10 @@ int realizarLogin()
     printf("Senha: ");
     scanf("%s", senha);
 
-    if (verificarLogin(usuarios, 2, nome, senha))
+    if (verificarLogin(usuarios, nome, senha))
     {
         printf("Login bem-sucedido. Bem-vindo, %s!\n", nome);
+        menuInicial(ehAdmin);
         return 1;
     }
     else
@@ -48,12 +108,44 @@ int realizarLogin()
     }
 
 }
+
 int main()
 {
-
     printf("Seja bem-vindo(a) a Eco Tech Solutions, para comecar vamos ao seu login:\n");
     realizarLogin();
     return 0;
 }
+
+void cadastrarIndustria()
+{
+    system("cls");
+    printf("Metodo de Cadastrar Industria.\n");
+}
+
+void atualizarDados()
+{
+    system("cls");
+    printf("Metodo de Atualizar Dados.\n");
+}
+
+void gerarRelatorios()
+{
+    system("cls");
+    printf("Metodo de Gerar Relatorios.\n");
+}
+
+void gerenciar()
+{
+    system("cls");
+    printf("Metodo de Gerenciamento.\n");
+}
+
+void cadastrarCliente()
+{
+    system("cls");
+    printf("Metodo de Cadastrar Cliente.\n");
+}
+
+
 
 
